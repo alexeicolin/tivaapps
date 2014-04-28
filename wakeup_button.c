ce44163035a0c8a45fa7c00b1959eaa4912e0afd
Alexei Colin <alexei.co@gmail.com>
2014-04-28 19:07:58 -0400

    main: safety button for debugger connect
    Conflicts:
    
    	Makefile

---
+++ wakeup_button.c
@@ -0,0 +1,17 @@
+#include <stdint.h>
+#include <xdc/std.h>
+
+#include <xdc/cfg/global.h>
+
+Void gpioButton1Fxn()
+{
+}
+
+Void gpioButton2Fxn()
+{
+    /* Safety: debugger has trouble connecting when program is (often) in deep
+     * sleep state that's clocked by PI or LFI. This should open up a chance
+     * for the debugger.
+     */
+    while (1);
+}
